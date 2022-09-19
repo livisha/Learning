97be12beeb578174921d6e41a68953584587716e
Livisha Jain <jainlivisha211@gmail.com>
2022-09-19 22:36:29 +0530

    ok

--- QuickKartDataAccessLayer/IRepository.cs
+++ QuickKartDataAccessLayer/IRepository.cs
@@ -8,10 +8,16 @@ namespace QuickKartDataAccessLayer
     public interface IRepository
     {
         public List<Categories> GetAllCategories();
+<<<<<<< Updated upstream
         public bool AddCategory(Categories category);
         public bool UpdateCategory(Categories category);
         public bool DeleteCategory(byte categID);
         public bool AddCategory(string categoryName);
         public byte GenerateNewCategoryId();
+=======
+        bool AddCategory(string categoryName);
+        void UpdateCategory(Categories category);
+        void DeleteCategory(byte categoryId);
+>>>>>>> Stashed changes
     }
 }
